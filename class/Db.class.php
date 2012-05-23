431f913c9c9a077354c87278eb30b4a9e7ecf42f
Michael Greenhill <doctorjbeam@gmail.com>
2012-05-24 09:03:52 +1000

    Modifications for Railpage
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- class/Db.class.php
+++ class/Db.class.php
@@ -1,13 +1,18 @@
-<?
+<?php
 /* this is nothing else but a PDO wrapper */
 class Db {
 	
 	private $pdoInstance;
    
     private static $instance;
+	
+	private $dbhost	= "cerium.railpage.org";
+	private $dbname	= "sparta";
+	private $dbuser	= "rpuser";
+	private $dbpass = "choochoo";
  
     private function __construct() {
-		$this->pdoInstance = new PDO("mysql:host=localhost;dbname=oauthProvider","oauth","oauth");
+		$this->pdoInstance = new PDO("mysql:host=".$this->dbhost.";dbname=".$this->dbname, $this->dbuser, $this->dbpass);
         $this->pdoInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         $this->pdoInstance->exec("set names 'utf8'");
 	}
--- client/index.php
+++ client/index.php
@@ -1,13 +1,20 @@
 <?php
-	$oauth_client = new Oauth("key","secret");
+	require_once("../../mainfile.php"); 
+	require_once("header.php");
+	
+	$oauth_client = new Oauth(API_PROVIDER_KEY, API_PROVIDER_SECRET);
 	$oauth_client->enableDebug();
 	try {
-		$info = $oauth_client->getRequestToken("http://localhost/OAuthProviderExample/oauth/request_token?oauth_callback=http://localhost/OAuthProviderExample/client/callback.php");
+		$info = $oauth_client->getRequestToken("http://cesium.railpage.org/oauth/oauth/request_token?oauth_callback=http://cesium.railpage.org/oauth/client/callback.php");
+		
+		#$User->oauth_key = $info['oauth_token'];
 		echo "<h1>We have a request token !</h1>";
 		echo "<strong>Request token</strong> : ".$info['oauth_token']."<br />";
 		echo "<strong>Request token secret</strong> : ".$info['oauth_token_secret']."<br />";
 		echo "to authenticate go <a href=\"".$info['authentification_url']."?oauth_token=".$info['oauth_token']."\">here</a>";
 	} catch(OAuthException $E){
-		echo "<pre>".print_r($E->debugInfo,true)."</pre>";
+		echo "<pre>".print_r($E->debugInfo, true)."</pre>";
 	}
+	
+	require_once("footer.php");
 ?>
